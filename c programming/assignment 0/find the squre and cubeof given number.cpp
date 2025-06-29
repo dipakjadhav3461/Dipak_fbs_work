a347c800bbd6af56bf2f75487233ae20dea23451
Dipak jadhav <147059441+dipakjadhav3461@users.noreply.github.com>
2025-06-29 17:50:19 +0530

    Add files via upload

---
+++ c programming/assignment 0/find the squre and cubeof given number.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+ {
+    int num = 45; 
+    int square, cube;
+    square = num * num;
+    cube = num * num * num;
+    printf("Number: %d\n", num);
+    printf("Square: %d\n", square);
+    printf("Cube: %d\n", cube);
+
+    return 0;
+}
